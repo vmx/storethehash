ee223fde0f70190978c6e3b6d9646cc210c7ffda
Volker Mische <volker.mische@gmail.com>
2020-11-27 17:26:39 +0100

    fix: improve error messages

--- src/error.rs
+++ src/error.rs
@@ -6,10 +6,10 @@ use crate::primary::PrimaryError;
 
 #[derive(Error, Debug)]
 pub enum Error {
-    #[error("IO Error.")]
+    #[error("IO Error: {0}")]
     Io(#[from] io::Error),
     #[error("Buckets error.")]
     BucketsOutOfBounds,
-    #[error("Primary storage error.")]
+    #[error("Primary storage error: {0}")]
     Primary(#[from] PrimaryError),
 }
