21488aeb515cc3854661cb670eb0f650ecb72357
Volker Mische <volker.mische@gmail.com>
2020-12-03 19:18:22 +0100

    feat: at validation command to fromcarfile example
    It's now possible to verify that the index has been created correctly.
    It checks that all keys are there and point to the right positions
    in the car file.

--- examples/fromcarfile/main.rs
+++ examples/fromcarfile/main.rs
@@ -4,6 +4,7 @@ use std::convert::TryFrom;
 use std::env;
 use std::fs::File;
 use std::io::{BufReader, Read, Seek, SeekFrom};
+use std::process::exit;
 
 use cid::Cid;
 use storethehash::index::Index;
@@ -57,21 +58,82 @@ fn insert_into_index<R: Read>(car_file: CarFile, car_iter: CarIter<R>, index_pat
     }
 }
 
+// Walk through the car file file and compare it with the data in the index.
+fn validate_index<R: Read>(
+    car_file: CarFile,
+    car_iter: CarIter<R>,
+    index_path: &str,
+) -> Result<(), (u64, Option<u64>)> {
+    const BUCKETS_BITS: u8 = 24;
+    let mut index = Index::<_, BUCKETS_BITS>::open(index_path, car_file).unwrap();
+
+    let mut counter = 0;
+    for (cid_bytes, _, pos) in car_iter {
+        if counter % 100000 == 0 {
+            println!("{} keys validated", counter);
+        }
+        let cid = Cid::try_from(&cid_bytes[..]).unwrap();
+        let digest = cid.hash().digest();
+
+        // Do nothing in case the positions match.
+        match index.get(&digest).unwrap() {
+            Some(pos_from_index) if pos_from_index != pos => {
+                return Err((pos, Some(pos_from_index)));
+            }
+            None => return Err((pos, None)),
+            _ => (),
+        }
+
+        counter += 1;
+    }
+
+    Ok(())
+}
+
 fn main() {
     fil_logger::init();
     let mut args = env::args().skip(1);
+    let command_arg = args.next();
     let car_path_arg = args.next();
     let index_path_arg = args.next();
-    match (car_path_arg, index_path_arg) {
-        (Some(car_path), Some(index_path)) => {
+    if let Some(command) = command_arg {
+        if let (Some(car_path), Some(index_path)) = (car_path_arg, index_path_arg) {
             let car_file_for_iter = File::open(&car_path).unwrap();
             let car_file_for_iter_reader = BufReader::new(car_file_for_iter);
             let car_iter = CarIter::new(car_file_for_iter_reader);
 
             let car_file_for_index = File::open(&car_path).unwrap();
             let primary_storage = CarFile::new(car_file_for_index);
-            insert_into_index(primary_storage, car_iter, &index_path);
+
+            match &command[..] {
+                "generate" => {
+                    insert_into_index(primary_storage, car_iter, &index_path);
+                    exit(0)
+                }
+                "validate" => match validate_index(primary_storage, car_iter, &index_path) {
+                    Ok(_) => {
+                        println!("Index is valid.");
+                        exit(0)
+                    }
+                    Err((primary_pos, Some(index_pos))) => {
+                        println!(
+                            "Invalid index: the index position `{}` \
+                            did not match the primary index position `{}`",
+                            index_pos, primary_pos
+                        );
+                        exit(1)
+                    }
+                    Err((primary_pos, None)) => {
+                        println!(
+                            "Invalid index: key not found, primary index position is `{}`",
+                            primary_pos
+                        );
+                        exit(1)
+                    }
+                },
+                _ => (),
+            }
         }
-        _ => println!("usage: fromcarfile <path-to-car-file> <index-file>"),
     }
+    println!("usage: fromcarfile [generate|validate] <path-to-car-file> <index-file>");
 }
